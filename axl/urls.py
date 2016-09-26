fcfadee7c25bf748ea3738598c798a77161448ac
Alex Gutierrez <alfegupe@gmail.com>
2016-09-26 12:05:25 -0500

    + Adjusted initial path for redirect to app.

--- axl/urls.py
+++ axl/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='bugtracker/')),
     url(r'^bugtracker/', include('bugtracker.urls')),
     url(r'^admin/', admin.site.urls, name='django_admin'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
